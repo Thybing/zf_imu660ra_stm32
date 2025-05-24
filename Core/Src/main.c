ab01a42643ee8add4bbbbbe0157607935a6a6869
Thybing <yanbinghao86@gmail.com>
2025-05-24 22:23:04 +0800

    添加姿态解算模块。

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,20 +18,19 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+
+#include "gpio.h"
 #include "i2c.h"
 #include "tim.h"
-#include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
+#include "attitude_algorithm/attitude_algorithm.h"
 #include "menu_nest/menu.h"
 #include "menu_nest_tiny/item_page_entrance.h"
 #include "menu_nest_tiny/item_static_variable.h"
 #include "menu_nest_tiny/page_base.h"
-
-#include "imu660ra/zf_device_imu660ra.h"
-
 #include "ssd1306/ssd1306.h"
 /* USER CODE END Includes */
 
@@ -69,150 +68,138 @@ void menu_init() {
     MN_menu_init(p_main_page);
 }
 
+
+Attitude_module attitude_module = {0};
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void) {
+    /* USER CODE BEGIN 1 */
+    /* USER CODE END 1 */
 
-  /* USER CODE BEGIN 1 */
+    /* MCU Configuration--------------------------------------------------------*/
 
-  /* USER CODE END 1 */
+    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+    HAL_Init();
 
-  /* MCU Configuration--------------------------------------------------------*/
+    /* USER CODE BEGIN Init */
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+    /* USER CODE END Init */
 
-  /* USER CODE BEGIN Init */
+    /* Configure the system clock */
+    SystemClock_Config();
 
-  /* USER CODE END Init */
+    /* USER CODE BEGIN SysInit */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+    /* USER CODE END SysInit */
 
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_TIM2_Init();
-  MX_I2C2_Init();
-  /* USER CODE BEGIN 2 */
-    menu_init();      // 初始化菜单
+    /* Initialize all configured peripherals */
+    MX_GPIO_Init();
+    MX_TIM2_Init();
+    MX_I2C2_Init();
+    /* USER CODE BEGIN 2 */
+    menu_init();          // 初始化菜单
     ssd1306_Init();       // 初始化OLED
     ssd1306_Fill(Black);  // 清屏
     imu660ra_init();
+    init_attitude(&attitude_module, 0.005f);
 
-    /*使能定时器中断*/
-    // HAL_TIM_Base_Start_IT(&htim2);  // 用来处理按键的定时器
+    HAL_TIM_Base_Start_IT(&htim2);
+    /* USER CODE END 2 */
 
-  /* USER CODE END 2 */
+    /* Infinite loop */
+    /* USER CODE BEGIN WHILE */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-    imu660ra_measurement_data_struct gyro_data = {0};
-    imu660ra_physical_data_struct gyro_phy_data = {0};
+    MN_item *p_var_rol = create_item_static_variable("roll", &attitude_module.pose_module.data.rol, TYPE_F32);
+    MN_item *p_var_pit = create_item_static_variable("pitch", &attitude_module.pose_module.data.pit, TYPE_F32);
+    MN_item *p_var_yaw = create_item_static_variable("yaw", &attitude_module.pose_module.data.yaw, TYPE_F32);
 
-    MN_item *p_var_gyro_x = create_item_static_variable("gyro_x", &gyro_phy_data.x, TYPE_F32);
-    MN_item *p_var_gyro_y = create_item_static_variable("gyro_y", &gyro_phy_data.y, TYPE_F32);
-    MN_item *p_var_gyro_z = create_item_static_variable("gyro_z", &gyro_phy_data.z, TYPE_F32);
-    int i = 0;
-    MN_item *p_i = create_item_static_variable("i", &i, TYPE_I32);
     MN_page *p_main_page = MN_get_main_page();
-    MN_page_add_item(p_main_page, p_var_gyro_x);
-    MN_page_add_item(p_main_page, p_var_gyro_y);
-    MN_page_add_item(p_main_page, p_var_gyro_z);
-    MN_page_add_item(p_main_page, p_i);
+    MN_page_add_item(p_main_page, p_var_rol);
+    MN_page_add_item(p_main_page, p_var_pit);
+    MN_page_add_item(p_main_page, p_var_yaw);
 
     while (1) {
-    /* USER CODE END WHILE */
+        /* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
-        imu660ra_get_gyro(&gyro_data);
-        imu660ra_get_physical_gyro(&gyro_data, IMU660RA_GYRO_RANGE_SGN_2000DPS, &gyro_phy_data);
+        /* USER CODE BEGIN 3 */
         MN_menu_handle_input_queue();  // 处理菜单的输入队列
         MN_menu_rendering();           // 渲染菜单
-        HAL_Delay(100);
     }
-  /* USER CODE END 3 */
+    /* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void) {
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
-  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL16;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    /** Initializes the RCC Oscillators according to the specified parameters
+     * in the RCC_OscInitTypeDef structure.
+     */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
+    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL16;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+        Error_Handler();
+    }
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+    /** Initializes the CPU, AHB and APB buses clocks
+     */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
+        Error_Handler();
+    }
 }
 
 /* USER CODE BEGIN 4 */
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
     static unsigned char ledState = 0;
+    if (htim->Instance == TIM2) {
+        update_attitude(&attitude_module);
+    }
 }
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void) {
+    /* USER CODE BEGIN Error_Handler_Debug */
     /* User can add his own implementation to report the HAL error return state */
     __disable_irq();
     while (1) {
     }
-  /* USER CODE END Error_Handler_Debug */
+    /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
-  /* USER CODE BEGIN 6 */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
+void assert_failed(uint8_t *file, uint32_t line) {
+    /* USER CODE BEGIN 6 */
     /* User can add his own implementation to report the file name and line number,
        ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+    /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
